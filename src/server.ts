e0bde9e00e4ed8c2e0fc936acff583224f0809be
kydinh <kydinh-udacity@gmail.com>
2023-04-05 11:08:37 +0700

    fix review from Udacity

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Errback } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Request, Response } from 'express';
 
 (async () => {
 
@@ -33,9 +34,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    try {
+      // image url
+      const image_url: string = req.query.image_url || '';
+      // validate the image_url query
+      if(image_url == '') {
+        console.log(`Invalid image_url`);
+        return res.status(400).json('Invalid image url');
+      }
+      console.log(`imageUrl: ${image_url}`);
+      // send the resulting file in the response
+      const image_path: string = await filterImageFromURL(image_url);
+      console.log(`imagePath: ${image_path}`);
+      return res.status(200).sendFile(image_path, async (err: Errback) => {
+          // deletes any files on the server on finish of the response
+          console.log(`deletes file on the server on finish of the response`);
+          await deleteLocalFiles([image_path]);
+          console.log(`deletes file sucessfully`);
+      });
+
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json("internal server!");
+    }
+  });
   
 
   // Start the Server
